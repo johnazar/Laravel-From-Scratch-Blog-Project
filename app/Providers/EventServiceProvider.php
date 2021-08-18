311f16f9ba5cb499d91795a7d61d107a8ed17fc6
John Azar (PC) <johnazar@users.noreply.github.com>
2021-08-18 09:47:22 +0300

    renaming

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -19,7 +19,7 @@ class EventServiceProvider extends ServiceProvider
             SendEmailVerificationNotification::class,
         ],
         \App\Events\PostPublishedEvent::class =>[
-            \App\Listeners\SendEmailPostPublishedNotification::class
+            \App\Listeners\SendEmailPostPublishedListener::class
         ]
     ];
 
