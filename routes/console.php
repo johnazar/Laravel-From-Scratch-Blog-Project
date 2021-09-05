904bd06a560ed691c8debdd96b4404ad9a0d0668
John Azar (PC) <johnazar@users.noreply.github.com>
2021-09-05 12:15:44 +0300

    add new command

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('inspire:log', function () {
+    Log::info(Inspiring::quote());
+})->purpose('Log an inspiring quote');
